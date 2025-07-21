6f6968f6e720e58450472cb39977a52df3be64bd
papa-docs <papa-docs1@proton.me>
2025-07-21 20:09:49 +0300

    Add files via upload

---
+++ FAKE/values.js
@@ -0,0 +1,70 @@
+// values.js
+
+// Основные данные 
+var fio               = "Iванченко Роман Iванович";
+var fio_en            = "Ivanchenko Roma";
+var birth             = "20.10.1985"; //дата рождения
+var date_give         = "28.08.2020"; //Дата видачи
+var date_out          = "28.08.2030"; // действителен до
+var organ             = "6351"; //орган что выдал документ
+var rnokpp            = "2877009110"; //ИНН
+var uznr              = "19851020-09616"; //Номер записи
+var pass_number       = "008420840"; //номер паспорта
+
+var registeredOn      = "03.11.2023"; //дата регистрации
+
+// Прописка
+var legalAdress       = "М. Красний Лиман"; //Место проживание
+var live              = "Красний Лиман, Донецкой область"; //Место рождение 
+var bank_adress       = "М. Красний Лиман"; //Место жительства указано в банке
+
+var sex               = "Ч";
+var sex_en            = "M";
+
+// Данные для Прав
+var rights_categories = "B"; //Категории
+var prava_number      = "AUX723727"; // номер прав
+var prava_date_give   = "04.09.2016"; //Дата выдачи Прав
+var prava_date_out    = "04.09.2036"; //Действителен ДО
+var pravaOrgan        = "1552"; //орган который выдал
+
+
+
+var university        = "ХНУ имени Каразина"; // Університет
+var fakultet          = "Физико-технический"; // Факультет
+var stepen_dip        = "Магістра";
+var univer_dip        = "ХНУ имени Каразина";
+var dayout_dip        = "01.07.2023";
+var special_dip       = "Прикладная математика";
+var number_dip        = "MT-545678";
+var form              = "Очная";
+
+
+// заграник
+var zagran_number     = "FZ317661"; //номер загран
+var dateGiveZ         = "28.08.2020"; //выдан загран
+var dateOutZ          = "28.08.2030"; //коньчаеться загран
+
+var student_number    = "2022154258";
+var student_date_give = "01.09.2021";
+var student_date_out  = "30.06.2025";
+
+
+
+// Включение/выключение документов
+var isRightsEnabled   = true;
+var isZagranEnabled   = true;
+var isDiplomaEnabled  = false;
+var isStudyEnabled    = false;
+// Добавь остальные переменные по аналогии
+
+// Пути к нужным фото    ?timestamp=N   "photo.jpg?timestamp=N" динамически: var url = "photo.jpg?timestamp=" + N;
+
+var photo_passport = "2.png"; //эДок, паспорт,
+var photo_rights   = "права.png"; //права
+var photo_students = "загран.png"; //диплом, студ
+var photo_zagran   = "загран.png"; //загран
+
+var signPng           = "sign.png"; //подпись
+// Можно добавить еще, если надо
+

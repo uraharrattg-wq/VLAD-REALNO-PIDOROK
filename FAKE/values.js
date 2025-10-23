f647fe3d9a788d89b36623cd91008f17b8a61376
papa-docs <papa-docs1@proton.me>
2025-10-23 20:38:31 +0300

    Update personal information in values.js

--- FAKE/values.js
+++ FAKE/values.js
@@ -1,32 +1,32 @@
 // values.js
 
 // Основные данные 
-var fio               = "Новiцький Богдан Олегович";
+var fio               = "Кандур Арсен Павлович";
 var fio_en            = "Novitskyi Bohdan";
-var birth             = "28.01.2007"; //дата рождения
+var birth             = "02.01.2007"; //дата рождения
 var date_give         = "18.11.2019"; //Дата видачи
-var date_out          = "18.11.2029"; // действителен до
-var organ             = "0512"; //орган что выдал документ
-var rnokpp            = "3582200550"; //ИНН
-var uznr              = "19980128-07937"; //Номер записи
-var pass_number       = "008420840"; //номер паспорта
+var date_out          = "08.09.2026"; // действителен до
+var organ             = "5149"; //орган что выдал документ
+var rnokpp            = "3582290450"; //ИНН
+var uznr              = "20070102-09771"; //Номер записи
+var pass_number       = "008196415"; //номер паспорта
 
 var registeredOn      = "20.09.1999"; //дата регистрации
 
 // Прописка
-var legalAdress       = "Вiнницька область"; //Место проживание
-var live              = "Вiнницька область"; //Место рождение 
-var bank_adress       = "Вiнницька область, вул. Паберемна, буд. 12, кв. 5"; //Место жительства указано в банке
+var legalAdress       = "М. Одеса, Одеська область"; //Место проживание
+var live              = "М. Одеса, Одеська область"; //Место рождение 
+var bank_adress       = "Вiнницька область, С. КОРСУНЦІ КОМІНТЕРНІВСЬКИЙ РАЙОН ОДЕСЬКА ОБЛАСТЬ ВУЛ. ЗАЛІЗНИЧНА БУД. 44"; //Место жительства указано в банке
 
 var sex               = "Ч";
 var sex_en            = "M";
 
 // Данные для Прав
 var rights_categories = "A, B"; //Категории
-var prava_number      = "AUX723027"; // номер прав
-var prava_date_give   = "01.04.2022"; //Дата выдачи Прав
-var prava_date_out    = "01.04.2032"; //Действителен ДО
-var pravaOrgan        = "0512"; //орган который выдал
+var prava_number      = "AUX753127"; // номер прав
+var prava_date_give   = "18.01.2025"; //Дата выдачи Прав
+var prava_date_out    = "18.01.2027"; //Действителен ДО
+var pravaOrgan        = "5149"; //орган который выдал
 
 
 
@@ -41,13 +41,13 @@ var form              = "Очная";
 
 
 // заграник
-var zagran_number     = "FX862524"; //номер загран
+var zagran_number     = "GE893359"; //номер загран
 var dateGiveZ         = "18.11.2019"; //выдан загран
 var dateOutZ          = "18.11.2029"; //коньчаеться загран
 
 var student_number    = "2022154258";
-var student_date_give = "01.09.2021";
-var student_date_out  = "30.06.2025";
+var student_date_give = "23.01.2023";
+var student_date_out  = "23.01.2033";
 
 
 
@@ -61,11 +61,12 @@ var isStudyEnabled    = false;
 // Пути к нужным фото    ?timestamp=N   "photo.jpg?timestamp=N" динамически: var url = "photo.jpg?timestamp=" + N;
 
 var photo_passport = "image_2025-10-22_14-44-50.png"; //эДок, паспорт,
-var photo_rights   = "image_2025-10-22_14-44-50.png"; //права
+var photo_rights   = "11221.png"; //права
 var photo_students = "image_2025-10-22_14-44-50.png"; //диплом, студ
 var photo_zagran   = "image_2025-10-22_14-44-50.png"; //загран
 
 var signPng           = "sign.png"; //подпись
 // Можно добавить еще, если надо
+
 
 
